5618b16eabcf93d775dac6203eba07ae8f123c98
Abbas Zainol <abbas.zainol@gmail.com>
2015-02-15 22:41:37 +0800

    From 10.10PM - 10.45PM (15/02/2015)

---
+++ ex1.rb
@@ -0,0 +1,19 @@
+puts "Hello, World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts "I print this just for you"
+
+print "Hello, world!"
+print "Hello again"
+print "I like typing this."
+print "This is fun."
+print "Yay! Printing."
+print "I'd much rather you 'not'"
+print 'I "said" do not touch this.'
+
+#puts will print new line for each statement.
+#print will print each statement in one line.
